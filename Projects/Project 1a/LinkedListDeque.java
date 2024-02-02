192d91affc62624a2b7bd3ed8735796df866759e
Qian Sun <69973222+Chin-Sun@users.noreply.github.com>
2024-02-02 00:33:12 -0500

    update Project 1

--- Projects/Project 1a/LinkedListDeque.java
+++ Projects/Project 1a/LinkedListDeque.java
@@ -11,44 +11,103 @@ public class LinkedListDeque <PlaceHolderType>{
         }
     }
 
-    /* The first item (if it exists) is at sentinel.next. */
+    /** Declare variables. */
     private ListNode sentinel;
     private int size;
 
-    /** Creates an empty DLList. */
+    /** Constructor:
+     * Creates an empty DLList. */
     public LinkedListDeque() {
         sentinel = new ListNode(null, null,null);
-        sentinel.next = new ListNode(null, null, null);
-        sentinel.prev = new ListNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         size = 0;
     }
-    public LinkedListDeque(PlaceHolderType x) {
-        sentinel = new StuffNode(null, null,null);
-        sentinel.next = sentinel.next.next.prev;
-        sentinel.prev = sentinel.prev.next;
-        size = 1;
-    }
 
 
-    /** Adds x to the front of the list. */
+    /**  Adds an item of type <PlaceholderType> x to the front of the deque.
+     *
+     *   Args:
+     *       item (PlaceholderType): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
     public void addFirst(PlaceHolderType x) {
-        sentinel.next = new StuffNode(sentinel, x, sentinel.next);
+        sentinel.next = new ListNode(sentinel, x, sentinel.next);
         sentinel.next.next.prev = sentinel.next;
         size = size + 1;
     }
 
-    /** Adds x to the end of the list. */
+    /**  Adds an item of type <PlaceholderType> to the back of the deque.
+     *
+     *   Args:
+     *       item (PlaceholderType): new first item.
+     *
+     *   Returns:
+     *       Nothing to return.
+     * */
     public void addLast(PlaceHolderType x) {
+        sentinel.prev = new ListNode(sentinel.prev, x, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
         size = size + 1;
+    }
+    /**  Returns true if deque is empty, false otherwise.*/
+    public boolean isEmpty(){
+        return (this.size == 0);
+    }
+    /**  Returns the number of items in the deque in constant time. */
+    public int size(){
+        return size;
+    }
 
-        ListNode p = sentinel;
-
-        /* Advance p to the end of the list. */
-        while (p.next != null) {
-            p = p.next;
+    /**  Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque(){
+        ListNode node = sentinel;
+        while(node != sentinel){
+            System.out.print(node.next.item+" ");
+            node = node.next;
         }
-
-        p.next = new StuffNode(sentinel, x, null);
+    }
+    /**  Removes and returns the item at the front of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       First item in Linked-List-Deque or null
+     * */
+    public PlaceHolderType removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        else {PlaceHolderType firstElement = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.next.prev = sentinel;
+        size = size-1;
+        return firstElement;
+        }
+    }
+    /**  Removes and returns the item at the back of the deque.
+     *   If no such item exists, returns null.
+     *
+     *   Args:
+     *       None
+     *
+     *   Returns:
+     *       First item in Linked-List-Deque or null
+     * */
+    public PlaceHolderType removeLast(){
+        if(size == 0){
+            return null;
+        } else{
+            PlaceHolderType lastElement = sentinel.prev.item;
+            sentinel.prev.prev.next = sentinel;
+            sentinel.prev = sentinel.prev.prev;
+            size -= 1;
+            return lastElement;
+        }
     }
 
     /** Returns the first item in the list. */
@@ -56,8 +115,101 @@ public class LinkedListDeque <PlaceHolderType>{
         return sentinel.next.item;
     }
 
-    /** Returns the size of the list. */
-    public int size() {
-        return size;
+    /**  Gets the item at the given index using <iteration>
+     *   where 0 is the front, 1 is the next item, and so forth.
+     *   If no such item exists, returns null.
+     *
+     *   <Must not alter the deque!>
+     *
+     *   Args:
+     *       i (int): index.
+     *
+     *   Returns:
+     *       index^{th} item in the deque.
+     * */
+    public PlaceHolderType getIterative(int index){
+        int length = size;
+        ListNode target = sentinel.next;
+
+        if(index > length - 1){
+            return null;
+        }else{
+            for (int i = 0; i < index; i++){
+                target = target.next;
+            }
+            return target.item;
+        }
     }
+    /**  Gets the item at the given index using <recursion>
+     *   where 0 is the front, 1 is the next item, and so forth.
+     *   If no such item exists, returns null.
+     *
+     *   <Must not alter the deque!>
+     *
+     *   Args:
+     *       i (int): index.
+     *
+     *   Returns:
+     *       index^{th} item in the deque.
+     * */
+    public PlaceHolderType getRecursive(int index){
+        if (index > size-1){
+            return null;
+        }else{
+            return recursive(sentinel.next, index);
+        }
+    }
+    /**  getRecursive helper function.
+     *
+     *   Args:
+     *       n (ListNode): current pointer.
+     *       i (int): index.
+     *
+     *   Returns:
+     *       target item (PlaceholderType).
+     * */
+    public PlaceHolderType recursive(ListNode n, int i){
+        if(i==0) {
+            return n.item;
+        }else{
+            return recursive(n.next, i-1);
+        }
+    }
+    /**  Dummy main function for test.
+     *
+     * */
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> Dllist = new LinkedListDeque<>();
+        Dllist.addFirst(666);
+        Dllist.addLast(6666);
+        Dllist.addLast(66666);
+        Dllist.printDeque();                        // expected (666 6666 66666)
+        System.out.println("Test getIterative #1");
+        System.out.println(Dllist.getIterative(0)); // expected 666
+        System.out.println(Dllist.getIterative(1)); // expected 6666
+        System.out.println(Dllist.getIterative(5)); // expected null
+        System.out.println("Test getIterative #1");
+        System.out.println(Dllist.getRecursive(0)); // expected 666
+        System.out.println(Dllist.getRecursive(1)); // expected 6666
+        System.out.println("Test done!");
+
+        Dllist.removeFirst();
+        Dllist.printDeque();                        // expected (6666 66666)
+        System.out.println("Test getIterative #2 removeFirst");
+        System.out.println(Dllist.getIterative(0)); // expected 6666
+        System.out.println(Dllist.getIterative(1)); // expected 66666
+        System.out.println("Test getRecursive #2 removeFirst");
+        System.out.println(Dllist.getRecursive(0)); // expected 6666
+        System.out.println(Dllist.getRecursive(1)); // expected 66666
+
+        Dllist.removeLast();
+        Dllist.printDeque();                        // expected 6666
+        System.out.println("Test getIterative #3 removeLast");
+        System.out.println(Dllist.getIterative(0)); // expected 6666
+        System.out.println(Dllist.getIterative(1)); // expected null
+        System.out.println("Test getRecursive #3 removeFirst");
+        System.out.println(Dllist.getRecursive(0)); // expected 6666
+        System.out.println(Dllist.getRecursive(1)); // expected null
+    }
+
 }
